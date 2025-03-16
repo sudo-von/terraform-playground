0165ad530859d9f5d8dd1ed923e89050600cbf32
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2025-03-16 17:35:09 -0600

    build: :bricks: update resource names

--- learn-docker-container/src/infrastructure/production/main.tf
+++ learn-docker-container/src/infrastructure/production/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "learn_docker_container_development" {
+resource "docker_image" "learn_docker_image_production" {
   name = var.docker_image_name
   build {
     context    = var.docker_image_dockerfile_context
@@ -18,9 +18,9 @@ resource "docker_image" "learn_docker_container_development" {
   }
 }
 
-resource "docker_container" "learn_docker_container_development_container" {
+resource "docker_container" "lear_docker_container_production" {
   name  = var.docker_container_name
-  image = docker_image.learn_docker_container_development.name
+  image = docker_image.learn_docker_image_production.name
   ports {
     internal = var.internal_port
     external = var.external_port
