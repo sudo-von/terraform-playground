d6a11ccccf53ca91787aa85b67bacb53e7cf1c16
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2025-03-16 17:31:43 -0600

    build: :bricks: add production infrastructure

---
+++ learn-docker-container/src/infrastructure/production/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "learn_docker_container_development" {
+  name = var.docker_image_name
+  build {
+    context    = var.docker_image_dockerfile_context
+    dockerfile = var.docker_image_dockerfile_name
+  }
+}
+
+resource "docker_container" "learn_docker_container_development_container" {
+  name  = var.docker_container_name
+  image = docker_image.learn_docker_container_development.name
+  ports {
+    internal = var.internal_port
+    external = var.external_port
+  }
+  restart = "unless-stopped"
+
+  volumes {
+    container_path = "/app"
+    host_path      = "${abspath(path.module)}/../../.."
+  }
+}
---
+++ learn-docker-container/src/infrastructure/production/variables.tf
@@ -0,0 +1,39 @@
+locals {
+  context = "../../.."
+}
+
+variable "docker_container_name" {
+  description = "Name of the container"
+  type        = string
+  default     = "learn-docker-container-production"
+}
+
+variable "docker_image_dockerfile_context" {
+  description = "The name of the Dockerfile"
+  type        = string
+  default     = "../../.."
+}
+
+variable "docker_image_dockerfile_name" {
+  description = "The name of the Dockerfile"
+  type        = string
+  default     = "Dockerfile.production"
+}
+
+variable "docker_image_name" {
+  description = "Name of the Docker image"
+  type        = string
+  default     = "learn-docker-container-production:latest"
+}
+
+variable "external_port" {
+  description = "External port of the local machine"
+  type        = number
+  default     = 3000
+}
+
+variable "internal_port" {
+  description = "Internal port of the container"
+  type        = number
+  default     = 3000
+}
